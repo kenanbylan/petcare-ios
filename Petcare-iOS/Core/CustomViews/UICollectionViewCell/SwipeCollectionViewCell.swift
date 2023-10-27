95c4f28d4073a50361766e69055a914ac91028b8
Kenan Baylan <kenan.baylan4654@gmail.com>
2023-10-27 11:14:40 +0300

    project font added.

---
+++ Petcare-iOS/Core/CustomViews/UICollectionViewCell/SwipeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SwipeCollectionViewCell.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 18.10.2023.
+//
+
+import UIKit
+
+class SwipeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Petcare-iOS/Helper/Constants/Font + Enum.swift
@@ -0,0 +1,8 @@
+//
+//  Font + Enum.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 18.10.2023.
+//
+
+import Foundation
