3b9d9439a0cd1587b195843d8c23172f70c8bbbf
Kenan Baylan <kenan.baylan4654@gmail.com>
2023-11-18 13:29:37 +0300

    [Kenan Baylan][GoogleButton & LoginButton & PatiButton new button components added]

---
+++ Petcare-iOS/Core/UIComponents/Buttons/LoginButton.swift
@@ -0,0 +1,42 @@
+//
+//  GoogleButton.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 15.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class LoginButton: UIButton {
+
+    // İlkleme (Initialization) işlemleri burada yapılabilir
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+
+    // for Storyboard or XIB use
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+
+    private func setupButton() {
+        setTitle("Login", for: .normal)
+        titleLabel?.font = AppFonts.font(for: .medium, size: 19)
+        backgroundColor = AppColors.primaryColor
+        tintColor = UIColor.darkText
+        layer.cornerRadius = 20
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowRadius = 1
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+        addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+
+    @objc private func buttonTapped() {
+        print("Login düğmesine tıklandı.")
+    }
+}
