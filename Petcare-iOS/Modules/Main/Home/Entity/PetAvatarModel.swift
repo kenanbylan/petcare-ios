15585d92bbc557580f309ca37eb2f238fb2d8918
Kenan Baylan <kenan.baylan4654@gmail.com>
2023-12-29 22:17:45 +0300

    [Kenan Baylan][HomeViewcontroller renew ]

---
+++ Petcare-iOS/Modules/Main/Home/Entity/PetAvatarModel.swift
@@ -0,0 +1,14 @@
+//
+//  PetAvatarModel.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 24.12.2023.
+//
+
+import Foundation
+
+struct PetAvatarModel {
+    let petImage: String?
+    let petName: String?
+    let petAge: String?
+}
