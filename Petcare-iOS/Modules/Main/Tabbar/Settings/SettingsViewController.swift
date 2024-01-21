6bb9b729b140569ad421587c2285e40fb1328c13
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-01-21 23:37:34 +0300

    [Kenan Baylan][SettingsViewController group tableView done]

--- Petcare-iOS/Modules/Main/Tabbar/Settings/SettingsViewController.swift
+++ Petcare-iOS/Modules/Main/Tabbar/Settings/SettingsViewController.swift
@@ -26,8 +26,8 @@ final class SettingsViewController: UIViewController {
     var models = [Section]()
     
     private let tableView: UITableView = {
-        let table = UITableView(frame: .zero, style: .grouped)
-        return table
+        let tableView = UITableView.init(frame: CGRect.zero, style: .grouped)
+        return tableView
     }()
     
     override func viewDidLoad() {
@@ -39,13 +39,18 @@ final class SettingsViewController: UIViewController {
     }
     
     private func prepareTableView() {
+        view.addSubview(tableView)
+        
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.backgroundColor = .clear
         tableView.frame = view.bounds
-        
+        tableView.layer.cornerRadius = 10
+        tableView.layer.masksToBounds = true
         tableView.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
         tableView.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
     }
+    
     
     private func configure() {
         //MARK: Switch Configuration
@@ -79,20 +84,70 @@ final class SettingsViewController: UIViewController {
 
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return models[section].options.count
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as! UITableViewCell
+        let model = models[indexPath.section].options[indexPath.row]
         
-        cell.backgroundColor = .blue
+        switch model.self {
+        case .staticCell(model: let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as? SettingTableViewCell else { return UITableViewCell() }
+            
+            cell.configureCell(with: model)
+            return cell
+            
+        case .switchCell(model: let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SwitchTableViewCell.identifier, for: indexPath) as? SwitchTableViewCell else { return UITableViewCell() }
+            
+            cell.configure(with: model)
+            return cell
+        }
         
-         
-        return cell
-
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = models[section]
+        return section.title
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == models.count - 1 {
+            return 60.0
+        }
+        return 0.0
     }
     
     
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footer = UIView()
+        footer.frame = CGRect(x: 0, y: 0, width: 540, height: 55)
+        if (section == models.count - 1){
+            footer.backgroundColor = .clear
+            let lbl = UILabel()
+            lbl.frame = CGRect(x: 10, y: 0, width: 540, height: 40)
+            lbl.backgroundColor = .clear
+            lbl.font = AppFonts.bold.font(size: 12)
+            lbl.text = "Created By Kenan Baylan"
+            lbl.numberOfLines = 1
+            footer.addSubview(lbl)
+            let lbl2 = UILabel()
+            lbl2.frame = CGRect(x: 10, y: 12, width: 540, height: 40)
+            lbl2.backgroundColor = .clear
+            lbl2.font = AppFonts.bold.font(size: 12)
+            lbl2.text = "Version 1.0.0"
+            lbl2.numberOfLines = 1
+            footer.addSubview(lbl2)
+            self.tableView.tableFooterView = footer
+
+        }
+            return footer
+    }
+
 }
