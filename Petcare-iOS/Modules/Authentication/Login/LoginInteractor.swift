9b7f3690453ee9038a86c55a91aa8ebc4a81e2af
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-07 22:13:54 +0300

    [Kenan Baylan][login auth updating and appdelegate comment remove]

--- Petcare-iOS/Modules/Authentication/Login/LoginInteractor.swift
+++ Petcare-iOS/Modules/Authentication/Login/LoginInteractor.swift
@@ -6,21 +6,43 @@
 //
 
 import Foundation
+import Combine
+import Network
 
 protocol LoginInteractorProtocol {
-    func checkUserLogin(email: String, password: String)
+    func checkUserLogin(onCompletion: @escaping(Bool) -> ())    
 }
 
 protocol LoginInteractorOutput {
     func internetConnectionStatus(_ status: Bool)
 }
 
-
-final class LoginInteractor : LoginInteractorProtocol {
+final class LoginInteractor: LoginInteractorProtocol, ObservableObject {
     var output: LoginInteractorOutput?
     
-    func checkUserLogin(email: String, password: String) {
-        
+    private let networkService: NetworkService
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init(networkService: NetworkService = NetworkManager()) {
+        self.networkService = networkService
     }
     
+    func checkUserLogin(onCompletion: @escaping(Bool) -> ()) {
+        let response: AnyPublisher<UserModel,APIError> = networkService.request(.signIn, headers: nil, parameters: nil)
+        response
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("Compoetion: \(completion)")
+                    
+                case .failure(let error):
+                    print("Error: \(error)")
+                    onCompletion(false)
+                }
+                
+            } receiveValue: { response in
+                print("response : \(response)")
+            }
+            .store(in: &cancellables)
+    }
 }
