70da8665497db480f929e198899c8419165d1103
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-07 22:12:08 +0300

    [Kenan Baylan][network layer some bug fixed and refactoring code]

---
+++ Petcare-iOS/Network/ErrorResponse.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kenan Baylan on 10.12.2023.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case apiError
+    case invalidEndpoint
+    case invalidRespons
+    case noData
+    case serializationError
+    case requestFailed(String)
+}
+
+enum ErrorResponse {
+    
+    case apiError
+    case invalidEndpoint
+    case invalidResponse
+    case noData
+    case serializationError
+    
+    public var description: String {
+        switch self {
+        case .apiError: return "There is something problem with the API"
+        case .invalidEndpoint: return "There is something problem with the Endpoint"
+        case .invalidResponse: return "There is something problem with the Response Model"
+        case .noData: return "Something problem with the Data"
+        case .serializationError: return "Problem with the Serialization Process"
+        }
+    }
+}
+
+//MARK:JSON verilerini çözümleme işlemi sırasında ortaya çıkan hatalar DecodingError tipi altında bulunabilir ve bu hatalar genellikle "serializationError" olarak adlandırılır.
---
+++ Petcare-iOS/Network/Network.swift
@@ -0,0 +1,106 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+import Combine
+
+@available(iOS 13.0, *)
+protocol NetworkService {
+    func request<T: Decodable>(_ endpoint: Endpoint, headers: [String: String]?, parameters: Encodable?) -> AnyPublisher<T, APIError>
+}
+
+@available(iOS 13.0, *)
+final class NetworkManager: NetworkService {
+    private let baseUrl: String
+    
+    init(environment: APIEnvironment = NetworkManager.defaultEnvironment()) {
+        self.baseUrl = environment.baseURL
+    }
+    
+    static func defaultEnvironment() -> APIEnvironment {
+#if DEBUG
+        return .development
+#elseif STAGING
+        return .staging
+#else
+        return .production
+#endif
+    }
+    
+    private func defaultHeaders() -> [String: String] {
+        var headers: [String: String] = [
+            "Platform" : "iOS",
+            "User-Token" : "userTokenExample",
+            "uid" : "user-id"]
+        
+        if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            headers["App-Version"] = appVersion
+        }
+        
+        return headers
+    }
+    
+    func request<T>(_ endpoint: Endpoint, headers: [String : String]?, parameters: Encodable?) -> AnyPublisher<T, APIError> where T : Decodable {
+        guard let url = URL(string: baseUrl + endpoint.path) else {
+            return Fail(error: APIError.invalidEndpoint).eraseToAnyPublisher()
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = endpoint.httpMethod.rawValue
+        
+        let allHeaders = defaultHeaders().merging(headers ?? [:], uniquingKeysWith: { (_, new) in new })
+        for (key, value) in allHeaders {
+            urlRequest.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        if let parameters = parameters {
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            do {
+                let jsonData = try JSONEncoder().encode(parameters)
+                urlRequest.httpBody = jsonData
+            } catch {
+                return Fail(error: APIError.requestFailed("Encoding parameters failed")).eraseToAnyPublisher()
+            }
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: urlRequest)
+            .tryMap { (data, response) -> Data in
+                if let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) {
+                    return data
+                } else {
+                    let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
+                    throw APIError.requestFailed("Request failed with status code: \(statusCode)")
+                }
+            }
+            .decode(type: ResponseWrapper<T>.self, decoder: JSONDecoder())
+            .tryMap { (responseWrapper) -> T in
+                guard let status = responseWrapper.status else {
+                    throw APIError.requestFailed("Missing data")
+                }
+                
+                switch status {
+                case 200:
+                    guard let data = responseWrapper.data else {
+                        throw APIError.requestFailed("Missing data.")
+                    }
+                    return data
+                default:
+                    let message = responseWrapper.message ?? "An error occurred."
+                    throw APIError.requestFailed(message)
+                }
+            }
+            .mapError { error -> APIError in
+                if error is DecodingError {
+                    return APIError.apiError
+                } else if let apiError = error as? APIError {
+                    return apiError
+                } else {
+                    return APIError.requestFailed("An unknown error occurred.")
+                }
+            }
+            .eraseToAnyPublisher()
+
+    }
+    
+}
+
