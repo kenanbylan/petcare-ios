cfc815dac953e33991bb6a801b56ff6a79b040d0
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-10 23:43:50 +0300

    [Kenan Baylan][ManageNotification file added]

---
+++ Petcare-iOS/Modules/Main/Tabbar/Settings/ManageNotification/ManageNotificationPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  ManageNotificationPresenter.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 10.02.2024.
+//
+
+import Foundation
---
+++ Petcare-iOS/Modules/Main/Tabbar/Settings/ManageNotification/ManageNotificationRouter.swift
@@ -0,0 +1,8 @@
+//
+//  ManageNotificationRouter.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 10.02.2024.
+//
+
+import Foundation
