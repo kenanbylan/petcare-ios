17dc55fb9e570bdc98672409c2a951217a142287
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-12 09:03:20 +0300

    [Kenan Baylan][BaseViewController added and project.pbxproj fixed]

--- Petcare-iOS/Core/BaseViewController.swift
+++ Petcare-iOS/Core/BaseViewController.swift
@@ -5,4 +5,19 @@
 //  Created by Kenan Baylan on 11.02.2024.
 //
 
-import Foundation
+import UIKit
+
+
+class BaseViewController: UIViewController {
+    
+    // Alt sınıfların bu metodu override edebilmesi için open olarak işaretlendi
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+    }
+    
+    fileprivate func setNavigationBar() {
+        navigationItem.setCustomBackButtonTitle("Back", color: AppColors.primaryColor)
+        navigationController?.navigationBar.tintColor = AppColors.primaryColor
+    }
+}
