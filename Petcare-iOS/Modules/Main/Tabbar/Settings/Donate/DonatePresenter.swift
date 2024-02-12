314354a585614e97a70bc5a12a3a8cc42856c40a
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-12 09:01:25 +0300

    [Kenan Baylan][Donate(buy a coffee) Folder list added]

--- Petcare-iOS/Modules/Main/Tabbar/Settings/Donate/DonatePresenter.swift
+++ Petcare-iOS/Modules/Main/Tabbar/Settings/Donate/DonatePresenter.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+protocol DonatePresenterProtocol {
+    func viewDidLoad()
+    func setTitle() -> String
+}
+
+final class DonatePresenter {
+    private weak var viewController: DonateViewController?
+    let router: DonateRouterProtocol?
+    let interactor: DonateInteractorProtocol?
+
+    //MARK: Variable's
+    var title:String = "Buy a Coffee"
+
+    init(viewController: DonateViewController? = nil, router: DonateRouterProtocol?, interactor: DonateInteractorProtocol?) {
+        self.viewController = viewController
+        self.router = router
+        self.interactor = interactor
+    }
+}
+extension DonatePresenter: DonatePresenterProtocol {
+    func viewDidLoad() {
+        
+    }
+    
+    func setTitle() -> String {
+        return title
+    }
+}
+
+extension DonatePresenter: DonateInteractorOutput {
+    
+}
+
+
