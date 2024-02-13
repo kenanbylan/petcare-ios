4bc482af8103d9021ad886aacedc7b2c322de1ea
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-13 23:10:10 +0300

    [Kenan Baylan][PetDetail Folder new controller created]
    viper arch modules added

---
+++ Petcare-iOS/Modules/Main/NewPetFlow/PetDetail/PetDetailInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  PetDetailInteractor.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 13.02.2024.
+//
+
+import Foundation
---
+++ Petcare-iOS/Modules/Main/NewPetFlow/PetDetail/PetDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PetDetailViewController.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 13.02.2024.
+//
+
+import UIKit
+
+class PetDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
