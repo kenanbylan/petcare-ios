140d82c4b2f10716e73baadefa0942f1ff45aace
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-25 13:11:49 +0300

    [Kenan Baylan][Auth Register refactoring]
    Interactor, Presenter, Router

--- Petcare-iOS/Modules/Authentication/Regşster/RegisterInteractor.swift
+++ Petcare-iOS/Modules/Authentication/Regşster/RegisterInteractor.swift
@@ -6,20 +6,47 @@
 //
 
 import Foundation
+import Combine
 
 protocol RegisterInteractorProtocol {
-    func getSignUpData() -> Void
+    func registerUser(name: String, surname: String, email: String, password: String) -> Void
 }
 
-protocol RegisterInteractorOutput {
-    func internetConnectionStatus(_ status: Bool)
+protocol RegisterInteractorOutput: AnyObject {
+    func registrationSuccess()
+    func registrationFailure(error: Error)
 }
-
 
 final class RegisterInteractor : RegisterInteractorProtocol {
-    var output: RegisterInteractorOutput?
     
-    func getSignUpData() {
-        
+    var subscriptions = Set<AnyCancellable>()
+
+    weak var output: RegisterInteractorOutput?
+    let networkManager: NetworkManager
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func registerUser(name: String, surname: String, email: String, password: String) {
+        do {
+            let requestData: [String: Any] = ["name": name, "surname": surname, "email": email,"password": password]
+            let jsonData = try JSONSerialization.data(withJSONObject: requestData)
+            networkManager.request(type: UserRegister.self , router: .register, method: .post, requestData: jsonData)
+                .sink { completion in
+                    switch completion {
+                    case .finished:
+                        break
+                    case .failure(let error):
+                        self.output?.registrationFailure(error: error)
+                    }
+                } receiveValue: { userData in
+                    print("User data: \(userData)")
+                    self.output?.registrationSuccess()
+                }
+                .store(in: &subscriptions)
+        } catch {
+            print("ERROR: \(error.localizedDescription)")
+        }
     }
 }
