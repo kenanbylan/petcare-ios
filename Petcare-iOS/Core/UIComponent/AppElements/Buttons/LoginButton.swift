a7f07d13ac3b23c8dd749b85705c0d1a12cb532f
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-02-25 13:12:53 +0300

    [Kenan Baylan][Button updated]
    Custom button, googlebutton, loginbutton, see all button.

---
+++ Petcare-iOS/Core/UIComponent/AppElements/Buttons/LoginButton.swift
@@ -0,0 +1,41 @@
+//
+//  GoogleButton.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 15.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class LoginButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+
+    // for Storyboard or XIB use
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+
+    private func setupButton() {
+        setTitle("Login", for: .normal)
+        titleLabel?.font = AppFonts.medium.font(size: 19)
+        backgroundColor = AppColors.primaryColor
+        tintColor = UIColor.darkText
+        layer.cornerRadius = 14
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowRadius = 1
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+        addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+
+    @objc private func buttonTapped() {
+        print("Login düğmesine tıklandı.")
+    }
+}
