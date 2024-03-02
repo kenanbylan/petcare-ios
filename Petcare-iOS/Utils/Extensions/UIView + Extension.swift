4b5329513d1a200c5f47ff863481325ce4bfac1a
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-03-02 10:45:45 +0300

    [Kenan Baylan][ UIView extension added]

--- Petcare-iOS/Utils/Extensions/UIView + Extension.swift
+++ Petcare-iOS/Utils/Extensions/UIView + Extension.swift
@@ -5,4 +5,18 @@
 //  Created by Kenan Baylan on 28.02.2024.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIView {
+func addShadow(shadowColor: CGColor = UIColor.black.cgColor,
+                   shadowOffset: CGSize = CGSize(width: 1.0, height: 2.0),
+                   shadowOpacity: Float = 0.4,
+                   shadowRadius: CGFloat = 3.0) {
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+        layer.masksToBounds = false
+    }
+}
