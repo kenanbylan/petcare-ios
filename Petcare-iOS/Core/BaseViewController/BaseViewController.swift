48f107249889c4865bcc5be12972a2ffff2117de
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-03-02 20:32:53 +0300

    [Kenan Baylan][ScrollableContentViewController and base view update]

--- Petcare-iOS/Core/BaseViewController/BaseViewController.swift
+++ Petcare-iOS/Core/BaseViewController/BaseViewController.swift
@@ -9,14 +9,36 @@ import UIKit
 
 
 class BaseViewController: UIViewController {
+    private var keyboardHeight: CGFloat = 0
+    
     open override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
         networkControl()
+        setupKeyboardNotifications()
+        viewDidTapped()
+    }
+    
+    
+    func viewDidTapped() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+           view.addGestureRecognizer(tapGesture)
+    }
+    
+    deinit {
+        removeKeyboardNotifications()
+        NotificationCenter.default.removeObserver(self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         networkControl()
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true) // Klavyeyi kapat
+    }
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     
@@ -46,4 +68,35 @@ class BaseViewController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+    
+    private func setupKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        
+        keyboardHeight = keyboardFrame.height
+        
+        // Override edilebilir method
+        keyboardWillShow(with: keyboardHeight)
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        keyboardHeight = 0
+        keyboardWillHide()
+    }
+    
+    @objc func keyboardWillShow(with height: CGFloat) {}
+    
+    @objc func keyboardWillHide() {
+         // ScrollView'in content inset'ini sıfırla
+    }
 }
