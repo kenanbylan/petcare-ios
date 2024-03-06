99178797b03079c1f51d60d60f65490023152723
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-03-06 21:21:14 +0300

    [Kenan Baylan][new extension added]

--- Petcare-iOS/Utils/Extensions/UICollectionView + Extension.swift
+++ Petcare-iOS/Utils/Extensions/UICollectionView + Extension.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+extension UICollectionView {
+    func registerNib(with identifier: String) {
+        self.register(UINib(nibName: identifier, bundle: Bundle.main), forCellWithReuseIdentifier: identifier)
+    }
+    
+    func registerCodedCell(with cellClass: AnyClass) {
+        let identifier = String(describing: cellClass.self)
+        self.register(cellClass, forCellWithReuseIdentifier: identifier)
+    }
+    
+    func dequeCell<T>(cellClass : T.Type, indexPath: IndexPath) -> T where T: UICollectionViewCell {
+        let identifier = String(describing: T.self)
+        return dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! T
+    }
+    
+}
