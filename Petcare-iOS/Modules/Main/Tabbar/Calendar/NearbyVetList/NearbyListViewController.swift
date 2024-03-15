967368b9b42160f1e3d1622a7040b0d8881a3e17
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-03-16 00:05:19 +0300

    nearbylist  created and distance settings

--- Petcare-iOS/Modules/Main/Tabbar/Calendar/NearbyVetList/NearbyListViewController.swift
+++ Petcare-iOS/Modules/Main/Tabbar/Calendar/NearbyVetList/NearbyListViewController.swift
@@ -6,35 +6,77 @@
 
 import UIKit
 import SwiftUI
+import MapKit
 
-
-protocol NearbyListViewProtocol: AnyObject {
-}
+protocol NearbyListViewProtocol: AnyObject { }
 
 final class NearbyListViewController: UIViewController {
     var presenter: NearbyListPresenterProtocol!
     var nearbyList = [NearbyList]()
+    var nearbyPlaces = [NearbyPlace]()
+    
+    var activityIndicator = UIActivityIndicatorView()
     
     private let tableView: UITableView = {
-        let tableView = UITableView.init(frame: .zero, style: .plain)
+        let tableView = UITableView.init(frame: .zero, style: .insetGrouped)
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nearbyList.append(.init(title: "Çınar Veteriner ", address: "Çınar Mahallesi 747.Sok no:16 daire:2 Bağcılar/İstanbul", userDistance: "340 M"))
-        nearbyList.append(.init(title: "liste1 ", address: "Çınar Mahallesi 747.Sok no:16 daire:2 Bağcılar/İstanbul", userDistance: "21 Km"))
-        nearbyList.append(.init(title: "liste1 ", address: "Çınar Mahallesi 747.Sok no:16 daire:2 Bağcılar/İstanbul", userDistance: "700 M"))
-        nearbyList.append(.init(title: "liste1 ", address: "Çınar Mahallesi 747.Sok no:16 daire:2 Bağcılar/İstanbul", userDistance: "2.3 Km" ))
-        nearbyList.append(.init(title: "Çınar Veteriner ", address: "Çınar Mahallesi 747.Sok no:16 daire:2 Bağcılar/İstanbul", userDistance: "340 M"))
-        nearbyList.append(.init(title: "Çınar Veteriner ", address: "Çınar Mahallesi 747.Sok no:16 daire:2 Bağcılar/İstanbul", userDistance: "340 M"))
-        nearbyList.append(.init(title: "Çınar Veteriner ", address: "Çınar Mahallesi 747.Sok no:16 daire:2 Bağcılar/İstanbul", userDistance: "340 M"))
-
-        
         presenter.viewDidLoad()
         prepareTableView()
         prepareTitle()
-        view.backgroundColor = .systemRed
+        getUserLocation()
+        prepareActivityIndicator()
+    }
+    
+    private func getUserLocation() {
+        activityIndicator.startAnimating()
+        LocationManager.shared.locationUpdated = { [weak self] location in
+            guard let self = self else { return }
+            print("Location \(location)")
+            self.fetchPlaces(location: location)
+        }
+    }
+    
+    private func fetchPlaces(location: CLLocationCoordinate2D) {
+        let searchSpan = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        let searchRegion = MKCoordinateRegion(center: location, span: searchSpan)
+        
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.region = searchRegion
+        searchRequest.resultTypes = .pointOfInterest
+        searchRequest.naturalLanguageQuery = "Veteriner"
+        
+        let search = MKLocalSearch(request: searchRequest)
+        
+        search.start { response, error in
+            guard let mapItems = response?.mapItems else { return }
+            self.nearbyPlaces.removeAll()
+            
+            for mapItem in mapItems {
+                let placeCoordinate = mapItem.placemark.coordinate
+                let placeLocation = CLLocation(latitude: placeCoordinate.latitude, longitude: placeCoordinate.longitude)
+                
+                let name = mapItem.name ?? "No name found"
+                let phoneNumber = mapItem.phoneNumber ?? "No phone number found"
+                let address = "\(mapItem.placemark.subThoroughfare ?? ""),\(mapItem.placemark.thoroughfare ?? ""), \(mapItem.placemark.locality ?? ""), \(mapItem.placemark.subLocality ?? ""), \(mapItem.placemark.administrativeArea ?? ""), \(mapItem.placemark.country ?? "")"
+                
+                let distance = "\(location.distance(to: placeLocation.coordinate))"
+                
+                ///MARK: Already list not append.
+                if !self.nearbyPlaces.contains(where: { $0.placeTitle == name }) {
+                    let nearbyPlace = NearbyPlace(placeTitle: name, address: address, phoneNumber: phoneNumber, distance: distance)
+                    self.nearbyPlaces.append(nearbyPlace)
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
     private func prepareTableView() {
@@ -45,6 +87,15 @@ final class NearbyListViewController: UIViewController {
         tableView.frame = view.bounds
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    private func prepareActivityIndicator() {
+        view.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
 }
 
@@ -57,17 +108,33 @@ extension NearbyListViewController: NearbyListViewProtocol {
 
 extension NearbyListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return nearbyList.count
+        return nearbyPlaces.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let list = nearbyList[indexPath.row]
+        let list = nearbyPlaces[indexPath.row]
         let cell = tableView.dequeCell(cellClass: NearbyListTableViewCell.self, indexPath: indexPath)
         cell.configureCell(with: list)
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UIScreen.screenWidth / 3
+        return UIScreen.screenWidth / 4
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let didSelectRow = nearbyPlaces[indexPath.row]
+        print("didSelectRow: \(didSelectRow)")
+        presenter.navigateToDetail(data: didSelectRow)
+    }
+    
+    //    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    //        let position = scrollView.contentOffset.y
+    //        if (position > tableView.contentSize.height - 100 - scrollView.frame.height) {
+    //            activityIndicator.startAnimating()
+    //        } else {
+    //            activityIndicator.stopAnimating()
+    //        }
+    //    }
+    
 }
