9147f1b20c271a1da1066f2fc39af3aba5e1718b
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-03-17 18:06:09 +0300

    [Kenan Baylan][horizontal label keyvalue stackView created]

---
+++ Petcare-iOS/Core/UIComponent/KeyValueStackView/KeyValueStackView.swift
@@ -0,0 +1,55 @@
+//
+//  KeyValueStackView.swift
+//  Petcare-iOS
+//
+//  Created by Kenan Baylan on 16.03.2024.
+//
+
+import Foundation
+import UIKit
+
+final class KeyValueStackView: UIStackView {
+    var data: [(String, String)]? {
+        didSet {
+            setupStackView()
+        }
+    }
+    
+    init(data: [(String, String)]? = nil) {
+        super.init(frame: .zero)
+        self.data = data
+        self.axis = .horizontal
+        self.alignment = .fill
+        self.spacing = 8
+        setupStackView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupStackView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        // Önceki stack view'ı temizle
+        self.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        guard let data = data else { return }
+        
+        // Her bir key-value çifti için
+        for (key, value) in data {
+            // Label'ler oluştur ve ayarla
+            let keyLabel = CustomLabel(text: "", fontSize: 17, fontType: .bold, textColor: AppColors.labelColor)
+            keyLabel.text = key
+            keyLabel.textAlignment = .left
+            
+            let valueLabel = CustomLabel(text: "", fontSize: 17, fontType: .medium, textColor: AppColors.labelColor)
+            valueLabel.text = value
+            valueLabel.textAlignment = .left
+            
+            // Label'leri stack view'a ekle
+            self.addArrangedSubview(keyLabel)
+            self.addArrangedSubview(valueLabel)
+        }
+    }
+}
+
