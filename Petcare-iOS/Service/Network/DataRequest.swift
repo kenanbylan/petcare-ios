158adf556a9bdb1c90e5ca5809133763334c1c57
Kenan Baylan <kenan.baylan4654@gmail.com>
2024-04-25 14:10:16 +0300

    [Kenan Baylan][Network layer backend system added]

--- Petcare-iOS/Service/Network/DataRequest.swift
+++ Petcare-iOS/Service/Network/DataRequest.swift
@@ -16,19 +16,20 @@ enum HTTPMethod: String {
 }
 
 protocol DataRequest {
-    associatedtype Response
+    associatedtype Response: Decodable
     
     var url: String { get }
     var method: HTTPMethod { get }
     var headers: [String : String] { get }
     var queryItems: [String : String] { get }
     var body: Data? { get }
-    func decode(_ data: Data) throws -> Response
+    func decode(_ data: Data) throws -> Response // New decoding method
 }
 
 extension DataRequest where Response: Decodable {
     func decode(_ data: Data) throws -> Response {
         let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase // Assuming snake_case JSON keys
         return try decoder.decode(Response.self, from: data)
     }
 }
--- Petcare-iOS/Service/Network/Network.swift
+++ Petcare-iOS/Service/Network/Network.swift
@@ -7,20 +7,41 @@
 import Foundation
 import UIKit
 
-protocol NetworkService {
-    func request<Request: DataRequest>(_ request: Request, completion: @escaping (Result<Request.Response, Error>) -> Void)
+enum NetworkError: Error {
+    case invalidURL
+    case requestFailed(Error)
+    case invalidResponse
+    case invalidData
+    case unauthorized // 401
+    case forbidden // 403
+    case notFound // 404
+    case badRequest // 400
+    case serverError // 5xx
+    case unknown(Int)
+    case status(Int) //
+}
+
+struct ExceptionErrorHandle: Codable, Error {
+    var ebusinessCode: String?
+    var businessErrorDetails: String?
+    var error: String?
+    
+    init(ebusinessCode: String?, businessErrorDetails: String?, error: String?) {
+        self.ebusinessCode = ebusinessCode
+        self.businessErrorDetails = businessErrorDetails
+        self.error = error
+    }
 }
 
 
+protocol NetworkService {
+    func request<Request: DataRequest>(_ request: Request, completion: @escaping (Result<Request.Response, ExceptionErrorHandle>) -> Void)
+}
+
 final class DefaultNetworkService: NetworkService {
-    func request<Request: DataRequest>(_ request: Request, completion: @escaping (Result<Request.Response, Error>) -> Void) {
+    func request<Request: DataRequest>(_ request: Request, completion: @escaping (Result<Request.Response, ExceptionErrorHandle>) -> Void) {
         guard var urlComponent = URLComponents(string: request.url) else {
-            let error = NSError(
-                domain: ErrorResponse.invalidEndpoint.rawValue,
-                code: 404,
-                userInfo: nil
-            )
-            return completion(.failure(error))
+            return completion(.failure(.init(ebusinessCode: "Invalid URL Component's",businessErrorDetails: nil,error: nil)))
         }
         
         var queryItems: [URLQueryItem] = []
@@ -40,36 +61,67 @@ final class DefaultNetworkService: NetworkService {
                 userInfo: nil
             )
             
-            return completion(.failure(error))
+            return completion(.failure(.init(ebusinessCode: "invalidEndpoint's",businessErrorDetails: nil,error: nil)))
         }
         
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = request.method.rawValue
-        urlRequest.allHTTPHeaderFields = request.headers
+        urlRequest.allHTTPHeaderFields = request.headers // header eklendi.
         urlRequest.httpBody = request.body
         urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type") // Content-Type ayarlandı
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
         
         
-        print("url request body: \(request.body)")
         URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             if let error = error {
-                return completion(.failure(error))
+                completion(.failure(.init(ebusinessCode: nil, businessErrorDetails: nil, error: error.localizedDescription)))
             }
             
-            guard let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode else {
-                return completion(.failure(NSError()))
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.init(ebusinessCode: nil, businessErrorDetails: nil, error: "Invalid http Response")))
+                return
             }
             
-            print("data: \(data)")
-            guard let data = data else {
-                return completion(.failure(NSError()))
-            }
-            
-            do {
-                try completion(.success(request.decode(data)))
-            } catch let error as NSError {
-                completion(.failure(error))
+            switch httpResponse.statusCode {
+                
+            case 200..<300:
+                guard let data = data else {
+                    completion(.failure(.init(ebusinessCode: nil, businessErrorDetails: nil, error: "Data decoding error")))
+                    return
+                }
+                do {
+                    try completion(.success(request.decode(data)))
+                } catch {
+                    completion(.failure(.init(ebusinessCode: nil, businessErrorDetails: nil, error: error.localizedDescription)))
+                }
+                
+            case 400:
+                guard let data = data else {
+                    completion(.failure(.init(ebusinessCode: "\(httpResponse.statusCode)", businessErrorDetails: nil, error: "Data decoding error")))
+                    return
+                }
+                
+                do {
+                    let exceptionErrorHandle = try JSONDecoder().decode(ExceptionErrorHandle.self, from: data)
+                    completion(.failure(exceptionErrorHandle))
+                } catch {
+                    completion(.failure(.init(ebusinessCode: "\(httpResponse.statusCode)", businessErrorDetails: nil, error: error.localizedDescription)))
+                }
+                
+            case 500..<600:
+                guard let data = data else {
+                    completion(.failure(.init(ebusinessCode: "\(httpResponse.statusCode)", businessErrorDetails: nil, error: "Data decoding error")))
+                    return
+                }
+                
+                do {
+                    let exceptionErrorHandle = try JSONDecoder().decode(ExceptionErrorHandle.self, from: data)
+                    completion(.failure(exceptionErrorHandle))
+                } catch {
+                    completion(.failure(.init(ebusinessCode: "\(httpResponse.statusCode)", businessErrorDetails: nil, error: error.localizedDescription)))
+                }
+                
+            default:
+                completion(.failure(.init(ebusinessCode: "\(httpResponse.statusCode)", businessErrorDetails: nil, error: "Default")))
             }
         }
         .resume()
